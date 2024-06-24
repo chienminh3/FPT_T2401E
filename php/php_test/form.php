f8525de796a9f2d77378f57a6abd5f40018a83da
chienminh3 <chienminh1703@gmail.com>
2024-06-25 01:57:30 +0700

    update work assignment and exam

--- php/php_test/form.php
+++ php/php_test/form.php
@@ -2,17 +2,14 @@
 require "dbconnect.php";
 $conn = $pdo;
 
-if (isset($_GET["delete"])) {
-    $id = $_GET["delete"];
-    delete($id, $conn);
-    header("location: list.php");
+if (isset($_GET["delete_id"])) {
+    $delete_id = $_GET["delete_id"];
+    delete($delete_id, $conn);
+    header("location: http://localhost/php_test/list.php");
     exit();
-} else {
-    echo "No ID provided for deletion.";
 }
 
 $id = $_REQUEST["id"] ?? -1;
-
 $data = getDataById($id, $conn);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -30,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // update
         update($id, $itemcode, $itemname, $quantity, $expireddate, $note, $conn);
     }
-    header("location: http://localhost/php_test/list.php");
+    header("location: list.php");
     exit();
 }
 
@@ -59,7 +56,7 @@ function getDataById($id, $conn)
         $stmt = $conn->prepare($sql);
         $stmt->bindParam("id", $id, PDO::PARAM_INT);
         $stmt->execute();
-        $data = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $data = $stmt->fetch(PDO::FETCH_OBJ);
         return $data;
     } catch (Exception $e) {
         echo $e->getMessage();
@@ -71,53 +68,59 @@ function update($id, $itemcode, $itemname, $quantity, $expireddate, $note, $conn
     try {
         $sql = "update item_sale set item_code= :item_code, item_name= :item_name, quantity= :quantity, expired_date= :expired_date, note= :note where id = :id";
         $stmt = $conn->prepare($sql);
-        $stmt->bindParam("id", $id, PDO::PARAM_INT);
-        $stmt->bindParam("item_code", $itemcode, PDO::PARAM_INT);
+        $stmt->bindParam("item_code", $itemcode, PDO::PARAM_STR);
         $stmt->bindParam("item_name", $itemname, PDO::PARAM_STR);
         $stmt->bindParam("quantity", $quantity, PDO::PARAM_STR);
         $stmt->bindParam("expired_date", $expireddate, PDO::PARAM_STR);
         $stmt->bindParam("note", $note, PDO::PARAM_STR);
+        $stmt->bindParam("id", $id, PDO::PARAM_INT);
+        $stmt->execute();
     } catch (PDOException $e) {
         echo "insert error " . $e->getMessage();
     }
 }
 
-function delete($id, $conn)
+function delete($delete_id, $conn)
 {
     try {
-        $sql = "delete item_sale where id= :id";
+        $sql = "delete from item_sale where id= :id";
         $stmt = $conn->prepare($sql);
-        $stmt->bindParam("id", $id, PDO::PARAM_INT);
+        $stmt->bindParam("id", $delete_id, PDO::PARAM_INT);
         $stmt->execute();
     } catch (Exception $e) {
         echo $e->getMessage();
     }
 }
+
+
 ?>
 
 
+<div style="width: 500px;height: auto;text-align: center;padding: 20px;box-sizing: border-box;border-radius: 10px;margin: 0 auto;">
+    <h1 style="color: orange;width: 100%;height: 50px;text-align: center;justify-content: center;background-color: green;">Form Sale Items</h1>   
 <form method="POST">
-    <input type="text" name="id" value='<?php echo !empty($data) ? $data->id : -1 ?>'>
-    <div>
-        <span>Item code</span>
-        <input type="text" name="itemcode" value='<?php echo !empty($data) ? $data->item_code : "" ?>'>
+    <input type="hidden" name="id" value='<?php echo !empty($data) ? $data->id : -1 ?>'>
+    <div style="width: 100%;display: flex;justify-content: space-between;margin-bottom: 10px;">
+        <span style="font-weight: bold;font-size: 30px;">Item code</span>
+        <input style="width: 250px;" type="text" name="itemcode" value='<?php echo !empty($data) ? $data->item_code : "" ?>'>
     </div>
-    <div>
-        <span>Item name</span>
-        <input type="text" name="itemname" value='<?php echo !empty($data) ? $data->item_name : "" ?>'>
+    <div style="width: 100%;display: flex;justify-content: space-between;margin-bottom: 10px;">
+        <span style="font-weight: bold;font-size: 30px;">Item name</span>
+        <input style="width: 250px;" type="text" name="itemname" value='<?php echo !empty($data) ? $data->item_name : "" ?>'>
     </div>
-    <div>
-        <span>Quantity</span>
-        <input type="text" name="quantity" value='<?php echo !empty($data) ? $data->quantity : "" ?>'>
+    <div style="width: 100%;display: flex;justify-content: space-between;margin-bottom: 10px;">
+        <span style="font-weight: bold;font-size: 30px;">Quantity</span>
+        <input style="width: 250px;" type="text" name="quantity" value='<?php echo !empty($data) ? $data->quantity : "" ?>'>
     </div>
-    <div>
-        <span>Expired date</span>
-        <input type="datetime-local" name="expireddate" value='<?php echo !empty($data) ? date("Y-m-d\Th:i:s", strtotime($data->expired_date)) : "" ?>'>
+    <div style="width: 100%;display: flex;justify-content: space-between;margin-bottom: 10px;">
+        <span style="font-weight: bold;font-size: 30px;">Expired date</span>
+        <input style="width: 250px;" type="datetime-local" name="expireddate" value='<?php echo !empty($data) ? date("Y-m-d\Th:i:s", strtotime($data->expired_date)) : "" ?>'>
     </div>
-    <div>
-        <span>Note</span>
-        <input type="text" name="note" value='<?php echo !empty($data) ? $data->note : "" ?>'>
+    <div style="width: 100%;display: flex;justify-content: space-between;margin-bottom: 10px;">
+        <span style="font-weight: bold;font-size: 30px;">Note</span>
+        <input style="width: 250px;" type="text" name="note" value='<?php echo !empty($data) ? $data->note : "" ?>'>
     </div>
-    <input type="submit" name="submit">
+    <button style="width: 100%;height: 50px;text-align: center;background-color: green;font-size: 20px;font-weight: bold;color: #fff;" type="submit">SUBMIT</button>
 
 </form>
+</div>
--- php/php_test/list.php
+++ php/php_test/list.php
@@ -1,45 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Document</title>
+</head>
 <?php
 require "dbconnect.php";
+
 try {
     $sql = "select * from item_sale";
-    $sttm = $pdo->prepare($sql);
-    $sttm->execute();
-    $data =  $sttm->fetchAll(PDO::FETCH_OBJ);
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $data =  $stmt->fetchAll(PDO::FETCH_OBJ);
 } catch (PDOException $e) {
     echo "get customer error " . $e->getMessage();
 }
 
 ?>
 
-
-<div style="width: 1000px;margin: 0 auto;">
-    <h1 style="width: 100%;height: 50px;justify-content: center;background-color: green;color: #fff;">V_Store items</h1>
-    <h1 style="width: 100%;height: 40px;text-align: center;background-color: green;color: orange;">Sale items</h1>
-    <a style="background-color: green;text-decoration: none;color: #fff; border-radius: 5px;" href="form.php"> Add New</a>
-    <table style="width: 1000px;">
-        <tr style="background-color: green;color: #fff;text-align: center;">
-            <td style="text-align: center;">Id</td>
-            <td style="text-align: center;">Item Code</td>
-            <td style="text-align: center;">Item Name</td>
-            <td style="text-align: center;">Quantity</td>
-            <td style="text-align: center;">Expired Date</td>
-            <td style="text-align: center;">Note</td>
-            <td></td>
-        </tr>
-
-        <?php foreach ($data as $value) : ?>
-            <tr>
-                <td style="text-align: center;"><?php echo $value->id ?></td>
-                <td style="text-align: center;"><?php echo $value->item_code ?></td>
-                <td style="text-align: center;"><?php echo $value->item_name ?></td>
-                <td style="text-align: center;"><?php echo $value->quantity ?></td>
-                <td style="text-align: center;"><?php echo $value->expired_date ?></td>
-                <td style="text-align: center;"><?php echo $value->note ?></td>
-                <td style="text-align: center;">
-                    <a href="form.php?id=<?php echo $value->id ?>">edit</a>
-                    <a href="form.php?delete=<?php echo $value->id ?>">delete</a>
-                </td>
+<body>
+    <div style="width: 1000px;margin: 0 auto;">
+        <h1 style="width: 100%;height: 50px;justify-content: center;background-color: green;color: #fff;">V_Store items</h1>
+        <h1 style="width: 100%;height: 40px;text-align: center;background-color: green;color: orange;">Sale items</h1>
+        <a href="form.php"><button style="width: 100px;height: 40px;background-color: green;border-radius: 5px;color: #fff;font-weight: bold;">Add new</button></a>
+        <table style="width: 1000px;">
+            <tr style="background-color: green;color: #fff;text-align: center;">
+                <td style="text-align: center;">Id</td>
+                <td style="text-align: center;">Item Code</td>
+                <td style="text-align: center;">Item Name</td>
+                <td style="text-align: center;">Quantity</td>
+                <td style="text-align: center;">Expired Date</td>
+                <td style="text-align: center;">Note</td>
+                <td></td>
             </tr>
-        <?php endforeach; ?>
-    </table>
-</div>
+
+            <?php foreach ($data as $value) : ?>
+                <tr>
+                    <td style="text-align: center;"><?php echo $value->id ?></td>
+                    <td style="text-align: center;"><?php echo $value->item_code ?></td>
+                    <td style="text-align: center;"><?php echo $value->item_name ?></td>
+                    <td style="text-align: center;"><?php echo $value->quantity ?></td>
+                    <td style="text-align: center;"><?php echo $value->expired_date ?></td>
+                    <td style="text-align: center;"><?php echo $value->note ?></td>
+                    <td style="text-align: center;">
+                        <a href="form.php?id=<?php echo $value->id ?>"><i style="font-size: 20px;color: goldenrod;" class="fa fa-pencil-square-o"></i></a>
+                        <a style="margin-left: 15px;" href="form.php?delete_id=<?php echo $value->id ?>"><i style="font-size: 20px;color: red;" class="fa fa-trash"></i></a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
+</body>
+
+</html>
